b64f25de7a95e4cbd389378002562a9e6d0ea018
colingalvin <colingalvin@ymail.com>
2020-10-30 09:00:33 -0500

    Finish logic for remaining API calls in AJAX.

--- Client/app.js
+++ Client/app.js
@@ -29,7 +29,7 @@
 
 // GET - Get all movies
 (function($){
-    function processForm2( e ){
+    function processForm( e ){
         $.ajax({
             url: 'https://localhost:44325/api/movie',
             dataType: 'json',
@@ -46,5 +46,82 @@
         e.preventDefault();
     }
 
-    $('#my-form-getAll').submit( processForm2 );
+    $('#my-form-getAll').submit( processForm );
+})(jQuery);
+
+// GET - Get single movie
+(function($){
+    function processForm( e ){
+        var id = 2;
+        $.ajax({
+            url: 'https://localhost:44325/api/movie/' + id,
+            dataType: 'json',
+            type: 'get',
+            contentType: 'application/json',
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( JSON.stringify(data) );
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        e.preventDefault();
+    }
+
+    $('#my-form-getSingle').submit( processForm );
+})(jQuery);
+
+// DELETE
+(function($){
+    function processForm( e ){
+        var id = 10;
+        $.ajax({
+            url: 'https://localhost:44325/api/movie/' + id,
+            dataType: 'json',
+            type: 'delete',
+            contentType: 'application/json',
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( textStatus );
+                console.log( textStatus );
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        e.preventDefault();
+    }
+
+    $('#my-form-delete').submit( processForm );
+})(jQuery);
+
+// PUT (update)
+(function($){
+    function processForm( e ){
+        var dict = {
+            MovieId : 5,
+        	Title : "Die Hard",
+            Director: "Brandon Prange",
+            Genre: "Action"
+        };
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'put',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( textStatus );
+                console.log( textStatus );
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        e.preventDefault();
+    }
+
+    $('#my-form-update').submit( processForm );
 })(jQuery);
