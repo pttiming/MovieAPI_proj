98f00f9ae09f08b991689315ff7dd7bd574d7aa8
pttiming <sean@pttiming.com>
2020-10-31 21:24:10 -0500

    bootstrap theme added. new background image, navbar dynamically populates the #response div. Set the image to display default value if image value is null

--- Client/app.js
+++ Client/app.js
@@ -1,13 +1,14 @@
 // Get all movies on load
-$(document).ready(viewAllMovies());
+//$(document).ready(viewAllMovies());
 
 // Button click events
 
-$('#my-form-getAll').submit(getAllMovies);
-$('#my-form-getSingle').submit(getSingleMovie);
+//$('#my-form-getAll').submit(getAllMovies);
+//$('#my-form-getSingle').submit(getSingleMovie);
+//$('#my-form-create1').submit(addMovie());
 $('#my-form-create').submit(addMovie);
-$('#my-form-update').submit(updateMovie);
-$('#my-form-delete').submit(deleteMovie);
+//$('#my-form-update').submit(updateMovie);
+//$('#my-form-delete').submit(deleteMovie);
 
 // API Calls
 
@@ -20,17 +21,27 @@ function viewAllMovies() {
         type: 'get',
         contentType: 'application/json',
         success: function( data, textStatus, jQxhr ){
+            $("#response").empty();
+            $("#response").html(
+                `<table style="margin-left:auto;margin-right:auto;" class="table-striped table-primary table-hover" id ="allMoviesTable" >
+        <tr>
+            <th></th>
+            <th>Title</th>
+            <th>Action</th>
+        </tr>`
+            )
             $.each(data, function(key, value){
                 $("#allMoviesTable").append(
-                    `<tr>
-                        <td><image src="${value.image}" style="width: 50px"></td>
+                    `<tr class="table-striped table-dark table-hover">
+                        <td><image src="${value.image}" onerror="this.src='./images/default.png'" style="width: 50px"></td>
                         <td>${value.title}</td>
                         <td>
                             <button class="details" onclick="getSingleMovie(${value.movieId})">Details</button>
                             <button class="edit" onclick="showInConsole(${value.movieId})">Edit</button>
-                            <button class="delete" onclick="showInConsole(${value.movieId})">Delete</button>
+                            <button class="delete" onclick="deleteMovie(${value.movieId})">Delete</button>
                         </td>
-                    </tr>`
+                    </tr>
+                    </table>`
                 );
             });
         },
@@ -53,7 +64,7 @@ function getAllMovies( e ){
         type: 'get',
         contentType: 'application/json',
         success: function( data, textStatus, jQxhr ){
-            console.log(data)
+            //console.log(data)
             //$('#response pre').html( JSON.stringify(data) );
         },
         error: function( jqXhr, textStatus, errorThrown ){
@@ -73,7 +84,26 @@ function getSingleMovie(id){
         contentType: 'application/json',
         success: function( data, textStatus, jQxhr ){
             console.log(data);
+            var image = data.image;
+            if(image == null){image = "./images/default.png"};
             //$('#response pre').html( JSON.stringify(data) );
+            $("#response").empty();
+            $("#response").html(
+                `<table style="margin-left:auto;margin-right:auto;" class="table-striped table-dark table-hover" id ="singleMovieTable" >
+            <tr>
+                <th></th>
+                <th>Title</th>
+                <th>Director</th>
+                <th>Genre</th>
+            </tr>
+            <tr>
+                        <td><image src="${image}" onerror="this.src='./images/default.png'" style="width: 50px"></td>
+                        <td>${data.title}</td>
+                        <td>${data.director}</td>
+                        <td>${data.genre}</td>
+            </tr>
+                </table>`
+            )
         },
         error: function( jqXhr, textStatus, errorThrown ){
             console.log( errorThrown );
@@ -84,9 +114,9 @@ function getSingleMovie(id){
 //Post - Add movie
 function addMovie( e ){
     var dict = {
-        Title : this["title"].value,
-        Director: this["director"].value,
-        Genre: this["genre"].value
+        Title : document.forms.my["title"].value,
+        Director: document.forms.my["director"].value,
+        Genre: document.forms.my["genre"].value
     };
 
     $.ajax({
@@ -149,4 +179,22 @@ function deleteMovie(id){
             console.log( errorThrown );
         }
     });
+}
+
+function homeClick(){
+    $("#response").empty()
+}
+
+function newMovie(){
+    $("#response").empty();
+    $("#response").append(
+        `<div><form name="my">
+        <input type="text" name="title" placeholder="Title" />
+        <input type="text" name="director" placeholder="Director" />
+        <input type="text" name="genre" placeholder="Genre" />
+        
+        <button type="submit" onclick="addMovie()">Create</button>
+    </form>
+</div>`
+    )
 }
